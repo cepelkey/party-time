7077855afaf8c6627dfc8acc974c8e0e3529941c
Charles Pelkey <40526951+cepelkey@users.noreply.github.com>
2018-06-26 21:45:08 -0400

    Added/Updated
    Added Dashboard code and template

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 require 'init.php';
 
 $app = new App();
-$app->add('HelloWorld');
 
-exit;
+$columns = $app->add('Columns');
+
+$left = $columns->addColumn();
+$right = $columns->addColumn();
+
+$right->add(['Message', 'Welcome to the Party, Pal!', 'info'])->text->addParagraph('Our party has a bring your own drink policy, so be sure to grab some beer or other adult beverage of your choice.');
+
+$left->add('Form')->setModel(new Guest($app->db));
--- init.php
+++ init.php
@@ -8,8 +8,37 @@ class App extends \atk4\ui\App {
         // Depending on the use, select appropriate layout
         if ($is_admin) {
             $this->initLayout('Admin');
+            $this->layout->menuLeft->addItem(['Dashboard', 'icon'=>'birthday cake'],['dashboard']);
+            $this->layout->menuLeft->addItem(['Guests', 'icon'=>'users'],['admin']);
         } else {
             $this->initLayout('Centered');
         }
+        $this->dbConnect('mysql://root:@localhost/party-time');
+    }
+}
+class Guest extends \atk4\data\Model {
+    public $table = 'guests';
+    function init() {
+        parent::init();
+
+        $this->addFields([
+            ['first_name', 'required'=>true],
+            ['last_name', 'required'=>true],
+            ['email', 'required'=>true],
+            'phone'
+        ]);
+        $this->addField('age', ['required'=>true]);
+        $this->addField('gender', ['enum'=>['male','female']]);
+        $this->addField('drink_qty', ['ui'=>['hint','BYOB Policy requires you to bring drinks!']]);
+    }
+}
+class Dashboard extends \atk4\ui\View {
+    public $defaultTemplate = __DIR__.'/dashboard.html';
+
+    function setModel($m){
+        $model = parent::setModel($m);
+        $this->template['guests'] = $model->action('count')->getOne();
+        $this->template['drinks'] = $model->action('fx',['sum','drink_qty'])->getOne();
+        return $model;
     }
 }
